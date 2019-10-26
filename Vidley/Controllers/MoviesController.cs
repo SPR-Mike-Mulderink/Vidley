144acf78fc35a0425d877f322f8cf98b4a88b006
Mulderink1 <mulderink.mike@gmail.com>
2019-10-25 20:37:26 -0500

    Added Custom Routes

--- Vidley/Controllers/MoviesController.cs
+++ Vidley/Controllers/MoviesController.cs
@@ -12,9 +12,38 @@ namespace Vidley.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek" };
+            var movie = new Movie() { Name = "Shrek!" };
+
+            //Old ViewData Approach
+            //ViewData["Movie"] = movie;
 
             return View(movie);
+
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortby = "name" });
+        }
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
